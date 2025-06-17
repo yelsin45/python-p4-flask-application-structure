1f21e2578f5cdb98062c1d54e8ab8818b3c162e1
yelsinkiprop <yelsin.kiprop@student.moringaschool.com>
2025-06-17 15:42:21 +0300

    all comments

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index ():
+    return '<h1>Welcome to my page!<h1>'
+
+@app.route('/<string:username>')
+def user (username):
+    return f'<h1>profile for {username}!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)    
